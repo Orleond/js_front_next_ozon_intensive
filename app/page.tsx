e92e2ef0dfef769aa8c7c8788775c604068aec52
Mr. Orleond <mr.orleond@gmail.com>
2025-11-21 21:47:53 +0300

    Day 6

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import { getData } from "./actions";
+import { Product } from "./models/product.model";
+import { Query } from "./models/query.model";
+
+export default async function Home({searchParams}: {searchParams: Query}) {
+  const query = await searchParams
+  const products = await getData(query)
+
+
   return (
     <div className="container">
       <div className="row">
@@ -35,8 +43,24 @@ export default function Home() {
         <div className="col-12 col-lg-9 col-xl-10">
           <div className="container">
             <div className="row no-gutters goods">
-              {/* <!-- КАРТОЧКИ ТОВАРОВ -->
-                  <!-- END КАРТОЧКИ ТОВАРОВ --> */}
+              {products.map((product: Product) => {
+                return (
+                  <div className="col-12 col-md-6 col-lg-4 col-xl-3" key={product.title}>
+                    <div className="card" data-key="${product.id}">
+                      {product.sale ? <div className="card-sale">🔥Hot Sale🔥</div> : null}
+                      <div className="card-img-wrapper">
+                        <span className="card-img-top"
+                          style={{backgroundImage: `url(${product.img})`}}></span>
+                      </div>
+                      <div className="card-body justify-content-between">
+                        <div className="card-price">{product.price} ₽</div>
+                        <h5 className="card-title">{product.title}</h5>
+                        <button className="btn btn-primary">В корзину</button>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
             </div>
           </div>
 
